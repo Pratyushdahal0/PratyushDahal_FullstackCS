ef89dab00bfe9252835638cb41c20c90d9122682
Pratyush <pratyushdahal33@gmail.com>
2026-02-02 09:07:50 +0545

    added sql query

--- config/restaurant.sql
+++ config/restaurant.sql
@@ -7,3 +7,21 @@ CREATE TABLE users (
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE orders (
+  id int(11) NOT NULL,
+  total_price decimal(10,2) NOT NULL,
+  status enum('Preparing','Prepared','Cancelled') NOT NULL DEFAULT 'Preparing',
+  created_at datetime DEFAULT current_timestamp()
+)
+CREATE TABLE order_items (
+    id INT AUTO_INCREMENT PRIMARY KEY,         
+    order_id INT NOT NULL,                      
+    dish_id INT NOT NULL,                        
+    dish_name VARCHAR(255) NOT NULL,            
+    price DECIMAL(10,2) NOT NULL,               
+    quantity INT NOT NULL,                       
+    subtotal DECIMAL(10,2) NOT NULL,           
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE
+);
+
